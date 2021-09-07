01fef55b92808924e7e45abe7628005fb7dc0807
jaime10a <jaimediezagero@gmail.com>
2021-09-08 01:31:23 +0200

    added language servers and some plugins

---
+++ lua/languageservers.lua
@@ -0,0 +1,13 @@
+--Setting Up the Language servers for the built in LSP
+--the Plugin lsp-config is needed for this
+--the language servers need to be installed on the system
+
+require'lspconfig'.pyright.setup{}
+
+require'lspconfig'.html.setup{}
+
+require'lspconfig'.texlab.setup{}
+
+require'lspconfig'.clangd.setup{}
+
+require'lspconfig'.sumneko_lua.setup{}
