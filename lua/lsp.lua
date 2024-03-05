8da9717a65affa5ae0ebab8e1580b91c64258855
jaime10a <jaimediezagero@gmail.com>
2024-03-05 21:38:38 +0100

    lsp config to separate file

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,21 +1,84 @@
---Setting Up the Language servers for the built in LSP
---the Plugin lsp-config is needed for this
---the language servers need to be installed on the system
+return { -- LSP Configuration & Plugins
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        { "j-hui/fidget.nvim", opts = {} }, --notifications
+    },
+    config = function()
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+            callback = function(event)
+                -- When you move your cursor, the highlights will be cleared (the second autocommand).
+                local client = vim.lsp.get_client_by_id(event.data.client_id)
+                if client and client.server_capabilities.documentHighlightProvider then
+                    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                        buffer = event.buf,
+                        callback = vim.lsp.buf.document_highlight,
+                    })
 
-require'lspconfig'.pyright.setup{}
+                    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                        buffer = event.buf,
+                        callback = vim.lsp.buf.clear_references,
+                    })
+                end
+            end,
+        })
 
-require'lspconfig'.html.setup{}
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities =
+            vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-require'lspconfig'.texlab.setup{}
+        local servers = {
+            clangd = {},
+            gopls = {},
+            pyright = {},
+            rust_analyzer = {},
+            -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+            lua_ls = {
+                -- cmd = {...},
+                -- filetypes { ...},
+                -- capabilities = {},
+                settings = {
+                    Lua = {
+                        runtime = { version = "LuaJIT" },
+                        workspace = {
+                            checkThirdParty = false,
+                            -- Tells lua_ls where to find all the Lua files that you have loaded
+                            -- for your neovim configuration.
+                            library = {
+                                "${3rd}/luv/library",
+                                unpack(vim.api.nvim_get_runtime_file("", true)),
+                            },
+                            -- If lua_ls is really slow on your computer, you can try this instead:
+                            -- library = { vim.env.VIMRUNTIME },
+                        },
+                        completion = {
+                            callSnippet = "Replace",
+                        },
+                    },
+                },
+            },
+        }
 
-require'lspconfig'.clangd.setup{}
+        require("mason").setup()
 
-require'lspconfig'.rust_analyzer.setup{}
+        local ensure_installed = vim.tbl_keys(servers or {})
+        vim.list_extend(ensure_installed, {
+            "stylua", -- Used to format lua code
+        })
+        require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-require'lspconfig'.gopls.setup{}
-
-require'lspconfig'.lua_ls.setup{}
-
-require'lspconfig'.svelte.setup{}
-
-require'lspconfig'.tsserver.setup{}
+        require("mason-lspconfig").setup({
+            handlers = {
+                function(server_name)
+                    local server = servers[server_name] or {}
+                    server.capabilities =
+                        vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+                    require("lspconfig")[server_name].setup(server)
+                end,
+            },
+        })
+    end,
+}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -243,96 +243,14 @@ require("lazy").setup(
             "karb94/neoscroll.nvim",
             config = function()
                 require("neoscroll").setup({
-                    hide_cursor = false, -- Hide cursor while scrolling
+                    hide_cursor = false,           -- Hide cursor while scrolling
                     easing_function = "quadratic", -- Default easing function
                 })
             end,
         },
 
-        { -- LSP Configuration & Plugins
-            "neovim/nvim-lspconfig",
-            dependencies = {
-                "williamboman/mason.nvim",
-                "williamboman/mason-lspconfig.nvim",
-                "WhoIsSethDaniel/mason-tool-installer.nvim",
-                { "j-hui/fidget.nvim", opts = {} }, --notifications
-            },
-            config = function()
-                vim.api.nvim_create_autocmd("LspAttach", {
-                    group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-                    callback = function(event)
-                        -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                        local client = vim.lsp.get_client_by_id(event.data.client_id)
-                        if client and client.server_capabilities.documentHighlightProvider then
-                            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                                buffer = event.buf,
-                                callback = vim.lsp.buf.document_highlight,
-                            })
-
-                            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                                buffer = event.buf,
-                                callback = vim.lsp.buf.clear_references,
-                            })
-                        end
-                    end,
-                })
-
-                local capabilities = vim.lsp.protocol.make_client_capabilities()
-                capabilities =
-                    vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-                local servers = {
-                    clangd = {},
-                    gopls = {},
-                    pyright = {},
-                    rust_analyzer = {},
-                    -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-                    lua_ls = {
-                        -- cmd = {...},
-                        -- filetypes { ...},
-                        -- capabilities = {},
-                        settings = {
-                            Lua = {
-                                runtime = { version = "LuaJIT" },
-                                workspace = {
-                                    checkThirdParty = false,
-                                    -- Tells lua_ls where to find all the Lua files that you have loaded
-                                    -- for your neovim configuration.
-                                    library = {
-                                        "${3rd}/luv/library",
-                                        unpack(vim.api.nvim_get_runtime_file("", true)),
-                                    },
-                                    -- If lua_ls is really slow on your computer, you can try this instead:
-                                    -- library = { vim.env.VIMRUNTIME },
-                                },
-                                completion = {
-                                    callSnippet = "Replace",
-                                },
-                            },
-                        },
-                    },
-                }
-
-                require("mason").setup()
-
-                local ensure_installed = vim.tbl_keys(servers or {})
-                vim.list_extend(ensure_installed, {
-                    "stylua", -- Used to format lua code
-                })
-                require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-                require("mason-lspconfig").setup({
-                    handlers = {
-                        function(server_name)
-                            local server = servers[server_name] or {}
-                            server.capabilities =
-                                vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-                            require("lspconfig")[server_name].setup(server)
-                        end,
-                    },
-                })
-            end,
-        },
+        --LSP config (in separate file)
+        require("lsp"),
 
         -- nvim-cmp Completion (in separate file)
         require("completion"),
