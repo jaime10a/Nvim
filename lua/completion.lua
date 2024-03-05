ddfc401ffac4496c828d38b8cabcd781a35d6c89
jaime10a <jaimediezagero@gmail.com>
2024-03-05 21:30:13 +0100

    Rewrote cmp with kickstarter. Formatting

--- lua/completion.lua
+++ lua/completion.lua
@@ -1,57 +1,91 @@
--- Setup nvim-cmp.
-local cmp = require'cmp'
+return {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        -- Snippet Engine & its associated nvim-cmp source
+        {
+            "L3MON4D3/LuaSnip",
+            build = "make install_jsregexp",
+        },
+        "saadparwaiz1/cmp_luasnip",
 
-cmp.setup({
-    snippet = {
-        expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-        end,
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/vim-vsnip",
+
+        "rafamadriz/friendly-snippets",
     },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-d>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
+    -- config = require('completion')
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        luasnip.config.setup({})
 
-  sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' },
-      { name = 'nvim_lua' },
-      { name = 'path' },
-    }, {
-      { name = 'buffer', keyword_length = 5 },
-    }),
-  experimental = {
-    ghost_text = false,
-  }
-})
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            completion = { completeopt = "menu,menuone,noinsert" },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                -- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),         -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<C-Space>"] = cmp.mapping.complete({}),
+                -- ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                -- ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                ["<C-e>"] = cmp.mapping.abort(),
 
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
-  })
+                -- Think of <c-l> as moving to the right of your snippet expansion.
+                --  So if you have a snippet that's like:
+                --  function $name($args)
+                --    $body
+                --  end
+                -- TODO this mapping has to change
+                -- <c-l> will move you to the right of each of the expansion locations.
+                -- <c-h> is similar, except moving you backwards.
+                ["<C-d>"] = cmp.mapping(function()
+                    if luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    end
+                end, { "i", "s" }),
+                ["<C-u>"] = cmp.mapping(function()
+                    if luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    end
+                end, { "i", "s" }),
+            }),
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "path" },
+                { name = "buffer",  keyword_length = 5 },
+            },
+            experimental = {
+                ghost_text = false,
+            },
+        })
+        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline({ "/", "?" }, {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = "buffer" },
+            },
+        })
 
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
-  })
-
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['rust_analyzer'].setup {
-    capabilities = capabilities
-  }
-  require('lspconfig')['pyright'].setup {
-    capabilities = capabilities
-  }
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = "path" },
+            }, {
+                { name = "cmdline" },
+            }),
+        })
+    end,
+}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,77 +12,64 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 require("lazy").setup(
 --Plugins
     {
 
         --Treesitter
         {
-            'nvim-treesitter/nvim-treesitter',
-            build = ':TSUpdate',
-            dependencies = 'romgrk/nvim-treesitter-context',
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            dependencies = "romgrk/nvim-treesitter-context",
             config = function()
-                require 'nvim-treesitter.configs'.setup {
+                require("nvim-treesitter.configs").setup({
                     ensure_installed = "all",
                     highlight = {
                         enable = true, -- false will disable the whole extension
                     },
-                }
-            end
+                })
+            end,
         },
 
         --Telescope
-        'nvim-lua/plenary.nvim',
-        'nvim-telescope/telescope.nvim',
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
         {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            build = 'make',
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
             config = function()
-                require('telescope').setup { extensions = fzf }
-                require('telescope').load_extension('fzf')
-            end
+                require("telescope").setup({ extensions = fzf })
+                require("telescope").load_extension("fzf")
+            end,
         },
 
         --Hop
         {
-            'phaazon/hop.nvim',
-            name = 'hop',
+            "phaazon/hop.nvim",
+            name = "hop",
             config = function()
-                require 'hop'.setup { keys = 'asdfjklqweruiopvmc' }
-            end
+                require("hop").setup({ keys = "asdfjklqweruiopvmc" })
+            end,
         },
 
         --Leap
         {
-            'ggandor/leap.nvim',
+            "ggandor/leap.nvim",
             config = function()
-                require('leap').set_default_keymaps()
-            end
-        },
-
-        -- nvim-cmp
-        {
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-nvim-lua",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "hrsh7th/cmp-vsnip",
-            "hrsh7th/vim-vsnip",
+                require("leap").set_default_keymaps()
+            end,
         },
 
         --AutoPairs
-        'windwp/nvim-autopairs',
+        "windwp/nvim-autopairs",
         -- require('nvim-autopairs').setup{}
 
         --Comment.nvim
         {
-            'numToStr/Comment.nvim',
+            "numToStr/Comment.nvim",
             config = function()
-                require('Comment').setup()
-            end
+                require("Comment").setup()
+            end,
         },
 
         -- SmartColumn
@@ -91,10 +78,16 @@ require("lazy").setup(
             opts = {
                 colorcolumn = "90",
                 disabled_filetypes = {
-                    "help", "text", "markdown", "mason", "lazy", "help", "NvimTree"
+                    "help",
+                    "text",
+                    "markdown",
+                    "mason",
+                    "lazy",
+                    "help",
+                    "NvimTree",
                 },
                 scope = "file",
-            }
+            },
         },
 
         --Tree
@@ -102,86 +95,84 @@ require("lazy").setup(
         --Quit_on_open option doesnt work.
         --It is being migrated to an option in lua
         {
-            'kyazdani42/nvim-tree.lua',
-            dependencies = 'kyazdani42/nvim-web-devicons',
+            "kyazdani42/nvim-tree.lua",
+            dependencies = "kyazdani42/nvim-web-devicons",
             config = function()
-                require 'nvim-tree'.setup {
+                require("nvim-tree").setup({
                     update_cwd = true,
                     update_focused_file = {
                         enable = true,
-                        update_cwd = true
+                        update_cwd = true,
                     },
-                }
-            end
+                })
+            end,
         },
 
         --Theme Nightfox
-        'EdenEast/nightfox.nvim',
+        "EdenEast/nightfox.nvim",
 
         -- Theme Gruvbox
-        'wittyjudge/gruvbox-material.nvim',
+        "wittyjudge/gruvbox-material.nvim",
 
         {
             "catppuccin/nvim",
             name = "catppuccin",
             config = function()
                 require("catppuccin").setup()
-            end
+            end,
         },
 
         --LuaLine
         {
-            'hoob3rt/lualine.nvim',
-            dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true }
+            "hoob3rt/lualine.nvim",
+            dependencies = { "kyazdani42/nvim-web-devicons", lazy = true },
         },
 
         --GitSigns
         {
-            'lewis6991/gitsigns.nvim',
+            "lewis6991/gitsigns.nvim",
             dependencies = {
-                'nvim-lua/plenary.nvim'
+                "nvim-lua/plenary.nvim",
             },
             config = function()
-                require('gitsigns').setup()
-            end
+                require("gitsigns").setup()
+            end,
         },
 
         --LazyGit
-        'kdheepak/lazygit.nvim',
+        "kdheepak/lazygit.nvim",
 
         --Neogit
         {
-            'TimUntersberger/neogit',
+            "TimUntersberger/neogit",
             dependencies = {
-                'nvim-lua/plenary.nvim',
-                'sindrets/diffview.nvim',
+                "nvim-lua/plenary.nvim",
+                "sindrets/diffview.nvim",
             },
             config = function()
-                require('neogit').setup {
+                require("neogit").setup({
                     disable_commit_confirmation = true,
                     integrations = {
-                        diffview = true
-                    }
-                }
-            end
+                        diffview = true,
+                    },
+                })
+            end,
         },
-
-
 
         --Startify Alpha-vim
         {
-            'goolord/alpha-nvim',
-            dependencies = { 'kyazdani42/nvim-web-devicons' },
+            "goolord/alpha-nvim",
+            dependencies = { "kyazdani42/nvim-web-devicons" },
             config = function()
-                require 'alpha'.setup(require 'alpha.themes.startify'.opts)
-            end
+                require("alpha").setup(require("alpha.themes.startify").opts)
+            end,
         },
 
         --Github Copilot with lua
         {
             "zbirenbaum/copilot.lua",
             config = function()
-                require('copilot').setup({
+                require("copilot").setup({
                     panel = {
                         auto_refresh = false,
                         keymap = {
@@ -194,7 +185,7 @@ require("lazy").setup(
                         layout = {
                             position = "bottom",
                             ratio = 0.3,
-                        }
+                        },
                     },
                     suggestion = {
                         enabled = true,
@@ -207,23 +198,21 @@ require("lazy").setup(
                             next = "<C-i>",
                             prev = false,
                             dismiss = false,
-                        }
-                    }
+                        },
+                    },
                 })
-            end
+            end,
         },
 
         --Latex Support
-        'lervag/vimtex',
+        "lervag/vimtex",
 
         --Whichkey
         {
             "folke/which-key.nvim",
             config = function()
-                require("which-key").setup {
-
-                }
-            end
+                require("which-key").setup({})
+            end,
         },
 
         --CodeGPT
@@ -235,52 +224,52 @@ require("lazy").setup(
 
         --ChatGPT.nvim
         {
-            'jackMort/ChatGPT.nvim',
+            "jackMort/ChatGPT.nvim",
 
             config = function()
-                require('chatgpt').setup({})
+                require("chatgpt").setup({})
             end,
             dependencies = {
-                'MunifTanjim/nui.nvim',
-                'nvim-lua/plenary.nvim',
-                'nvim-telescope/telescope.nvim'
-            }
+                "MunifTanjim/nui.nvim",
+                "nvim-lua/plenary.nvim",
+                "nvim-telescope/telescope.nvim",
+            },
         },
         {
             "folke/trouble.nvim",
         },
         --Neoscroll
         {
-            'karb94/neoscroll.nvim',
+            "karb94/neoscroll.nvim",
             config = function()
-                require('neoscroll').setup({
-                    hide_cursor = false,          -- Hide cursor while scrolling
-                    easing_function = "quadratic",       -- Default easing function
+                require("neoscroll").setup({
+                    hide_cursor = false, -- Hide cursor while scrolling
+                    easing_function = "quadratic", -- Default easing function
                 })
-            end
+            end,
         },
 
         { -- LSP Configuration & Plugins
-            'neovim/nvim-lspconfig',
+            "neovim/nvim-lspconfig",
             dependencies = {
-                'williamboman/mason.nvim',
-                'williamboman/mason-lspconfig.nvim',
-                'WhoIsSethDaniel/mason-tool-installer.nvim',
-                { 'j-hui/fidget.nvim', opts = {} }, --notifications
+                "williamboman/mason.nvim",
+                "williamboman/mason-lspconfig.nvim",
+                "WhoIsSethDaniel/mason-tool-installer.nvim",
+                { "j-hui/fidget.nvim", opts = {} }, --notifications
             },
             config = function()
-                vim.api.nvim_create_autocmd('LspAttach', {
-                    group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+                vim.api.nvim_create_autocmd("LspAttach", {
+                    group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
                     callback = function(event)
                         -- When you move your cursor, the highlights will be cleared (the second autocommand).
                         local client = vim.lsp.get_client_by_id(event.data.client_id)
                         if client and client.server_capabilities.documentHighlightProvider then
-                            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
                                 buffer = event.buf,
                                 callback = vim.lsp.buf.document_highlight,
                             })
 
-                            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
                                 buffer = event.buf,
                                 callback = vim.lsp.buf.clear_references,
                             })
@@ -289,7 +278,8 @@ require("lazy").setup(
                 })
 
                 local capabilities = vim.lsp.protocol.make_client_capabilities()
-                capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+                capabilities =
+                    vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
                 local servers = {
                     clangd = {},
@@ -303,48 +293,49 @@ require("lazy").setup(
                         -- capabilities = {},
                         settings = {
                             Lua = {
-                                runtime = { version = 'LuaJIT' },
+                                runtime = { version = "LuaJIT" },
                                 workspace = {
                                     checkThirdParty = false,
                                     -- Tells lua_ls where to find all the Lua files that you have loaded
                                     -- for your neovim configuration.
                                     library = {
-                                        '${3rd}/luv/library',
-                                        unpack(vim.api.nvim_get_runtime_file('', true)),
+                                        "${3rd}/luv/library",
+                                        unpack(vim.api.nvim_get_runtime_file("", true)),
                                     },
                                     -- If lua_ls is really slow on your computer, you can try this instead:
                                     -- library = { vim.env.VIMRUNTIME },
                                 },
                                 completion = {
-                                    callSnippet = 'Replace',
+                                    callSnippet = "Replace",
                                 },
                             },
                         },
                     },
                 }
 
-                require('mason').setup()
+                require("mason").setup()
 
                 local ensure_installed = vim.tbl_keys(servers or {})
                 vim.list_extend(ensure_installed, {
-                    'stylua', -- Used to format lua code
+                    "stylua", -- Used to format lua code
                 })
-                require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+                require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-                require('mason-lspconfig').setup {
+                require("mason-lspconfig").setup({
                     handlers = {
                         function(server_name)
                             local server = servers[server_name] or {}
-                            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities,
-                                server.capabilities or {})
-                            require('lspconfig')[server_name].setup(server)
+                            server.capabilities =
+                                vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+                            require("lspconfig")[server_name].setup(server)
                         end,
                     },
-                }
+                })
             end,
         },
 
-
+        -- nvim-cmp Completion (in separate file)
+        require("completion"),
     },
 
     --Options for Lazyvim
@@ -353,6 +344,5 @@ require("lazy").setup(
             path = "~/projects/nvim",
             fallback = false, -- Fallback to git when local plugin doesn't exist
         },
-
-
-    })
+    }
+)
